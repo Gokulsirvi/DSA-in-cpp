c6e83d95708036b8b6b5918aaa97fe5e0acdccd3
Gokul sirvi <gokulsirvi6@gmail.com>
2025-09-15 08:31:25 +0530

    Create binarySearchTree.cpp

---
+++ DataStructure/binarySearchTree.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+  //  int height;
+    Node(int val){
+        data=val;
+        left= right= nullptr;
+    }
+};
+ Node* insert(Node* root,int val){
+    if(root==nullptr){
+        return new Node(val);
+    else if(val<root->data){
+       root->left = insert(root->left,vaL);
+    }
+    else if(val>root->data){
+        root->right = insert(root->right,vaL);
+    }
+    return root;
+}
+void inorder(Node* root){
+    if(root){
+      inorder(root->left);
+      cout<<root->data<<" ";
+      inorder(root->right);
+    }
+}
+int main() {
+    Node* root = nullptr;
+    int n;
+    cout<<"Enter total node = ";
+    cin>>n;
+    int *arr = new int[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        root = insert(root,arr[i]);
+    }
+    cout<<endl;
+    inorder(root);
+    return 0;
+}
